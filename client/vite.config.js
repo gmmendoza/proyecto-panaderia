007ada43cbc50b1c15bf971ff71c37e1de13ee65
SGT <guuamendoza@gmail.com>
2026-03-16 17:46:28 -0300

    Fix: set explicit base path for GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/proyecto-panaderia/",
   plugins: [react()],
   server: {
     proxy: {
